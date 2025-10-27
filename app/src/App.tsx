bc8e974d68850301c498c07a57642e8a172c4f2c
Hiroaki Tahara <taharahiroaki@machiro.local>
2025-10-27 23:58:00 +0900

    layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,6 +9,8 @@ import {
 } from "@mui/material";
 import "./App.css";
 import BTree from "./component/BTreePage/BTree";
+import Editor from "./component/Editor";
+import Datatable from "./component/Datatable";
 
 function App() {
   return (
@@ -28,18 +30,30 @@ function App() {
         </Toolbar>
       </AppBar>
       <Grid container spacing={2}>
-        <Grid size={6}>
-          <Paper elevation={3} />
-          <div>Hello</div>
+        <Grid size={6} mt={2}>
+          <Paper elevation={3}>
+            <Editor />
+          </Paper>
         </Grid>
-        <Grid size={6}>
-          <Paper elevation={3} />
-          <div>world</div>
+        <Grid size={6} mt={2}>
+          <Paper elevation={3}>
+            <Datatable />
+          </Paper>
         </Grid>
-        <Grid size={12}>
-          <Box display="flex">
-            <BTree />
-          </Box>
+        <Grid size={12} mt={1}>
+          <Paper elevation={3} sx={{ minHeight: "50vh" }}>
+            <Box
+              sx={{
+                margin: 0,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingTop: "100px",
+              }}
+            >
+              <BTree />
+            </Box>
+          </Paper>
         </Grid>
       </Grid>
     </Box>
--- app/src/component/Editor.tsx
+++ app/src/component/Editor.tsx
@@ -1,1 +1,33 @@
-export default function Node() {}
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+export default function Editor() {
+  return (
+    <Card sx={{ height: "300px" }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          hello
+        </Typography>
+        <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
